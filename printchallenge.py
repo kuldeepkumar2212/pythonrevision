1ecf3c13cef23f05d5c5713dc9fd3cbf499b5dc9
kuldeepkumar2212 <kuldeepkumarraju2001@gmail.com>
2026-08-12 11:11:44 +0530

    python challenge: print

---
+++ printchallenge.py
@@ -0,0 +1,10 @@
+'''problem statement: Write a python program to print the following output using a single print statement. 
+Your learning path:
+        - python Basics 
+        - Data Engineering 
+        - Ai'''
+        
+print ('''Your learning path:
+    \t- python Basics 
+    \t- Data Engineering 
+    \t- Ai''')
