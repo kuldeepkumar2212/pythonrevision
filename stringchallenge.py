202caba41bcd48d748972e9a48cf29a4aefea7a6
kuldeepkumar2212 <kuldeepkumarraju2001@gmail.com>
2026-08-24 10:42:03 +0530

    python challenge: strings

---
+++ stringchallenge.py
@@ -0,0 +1,10 @@
+#"968-Maria, ( D@t@ Enfineer );; 27y  " clean this string to get the following 
+# output: "name: maria | role: Data Engineer | age:27y 
+
+text = "968-,Maria ( D@t@ Enfineer );; 27y  "
+
+name = text.split('-')[1].split(',')[0].strip()
+role = text.split('(')[1].split(')')[0].replace('@', '').strip()
+age = text.split(';;')[1].strip()
+
+print(f"name: {name} | role: {role} | age: {age}")
