d30bddc0ccb663a3ca2ee8ecc72a59cc6e1a3448
kuldeepkumar2212 <kuldeepkumarraju2001@gmail.com>
2026-08-25 11:06:35 +0530

    python challenge: loops and for else loops

--- loopschallenge.py
+++ loopschallenge.py
@@ -1,6 +1,28 @@
 #print the n table from 1 to 10 using loops
 
-n = input ("enter the nu,ber for which you want to print the table:")
+#n = input ("enter the nu,ber for which you want to print the table:")
 
-for i in range(1,11):
-    print(f"{n} * {i} = {int(n)*i}")
+#for i in range(1,11):
+#    print(f"{n} * {i} = {int(n)*i}")
+     
+#print the left aligned pyramid of stars using loops as below if n = 3
+# *
+# **
+# ***
+
+#n = int(input ("enter the nuber of rows:"))
+#for i in range(n):
+#    for j in range(i+1):
+#        print("*", end="")
+#    print()
+
+#check if the file name appers more than once in the list of files using loops
+
+files = ['report.docx', 'data.csv', 'presentation.pptx', 'report1.docx', 'summary.pdf']
+
+for file in files:
+    if files.count(file) > 1:
+        print(f"{file} appears more than once in the list.")
+        break
+else:
+    print("All files are unique in the list.")
