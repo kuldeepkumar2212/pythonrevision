7a2d7ca944376c81ef2bdc3349e0d8cb7a0669c9
kuldeepkumar2212 <kuldeepkumarraju2001@gmail.com>
2026-08-25 11:17:47 +0530

    python challenge: while loops

--- loopschallenge.py
+++ loopschallenge.py
@@ -18,11 +18,31 @@
 
 #check if the file name appers more than once in the list of files using loops
 
-files = ['report.docx', 'data.csv', 'presentation.pptx', 'report1.docx', 'summary.pdf']
+#files = ['report.docx', 'data.csv', 'presentation.pptx', 'report1.docx', 'summary.pdf']
 
-for file in files:
-    if files.count(file) > 1:
-        print(f"{file} appears more than once in the list.")
+#for file in files:
+#    if files.count(file) > 1:
+#        print(f"{file} appears more than once in the list.")
+#        break
+#else:
+#    print("All files are unique in the list.")
+
+#to get an yes input for the user using loops
+#1. allow only 3 attempts to get a valid input
+#2. if the user fails to provide a valid input after 3 attempts, print "You have exceeded the maximum number of attempts."
+#3. if the user provides a valid input, print "Thank you for your response."
+
+n = 3
+
+while n > 0: 
+    response = input("Please enter 'yes' or 'no': ").lower()
+    if response == 'yes':
+        print("Thank you for your response.")
         break
-else:
-    print("All files are unique in the list.")
+    else:
+        n -= 1
+        if n == 0:
+            print("You have exceeded the maximum number of attempts.")
+        else:
+            print(f"Invalid input enter yes. You have {n} attempts left.") 
+            
